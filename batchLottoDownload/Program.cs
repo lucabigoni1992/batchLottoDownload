78127c85651984eb36528de8c86af10f645505f9
luca <lucabig92@live.it>
2020-05-01 17:11:10 +0200

    Revert "removerd not used source"
    This reverts commit 274a1a702461240f46707b29d2c70ead314c52cb.

---
+++ batchLottoDownload/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using libraryLotto;
+namespace batchLottoDownload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            batchDownloadData lottoData = new batchDownloadData();
+            lottoData.downloadAllLotto();
+        }
+    }
+}
