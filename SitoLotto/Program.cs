fb651f29495c4540e0b14c50a36a08022e9009e5
luca <lucabig92@live.it>
2020-05-02 14:57:17 +0200

    sss

--- SitoLotto/Program.cs
+++ SitoLotto/Program.cs
@@ -21,6 +21,10 @@ namespace SitoLotto
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    var p = System.Reflection.Assembly.GetEntryAssembly().Location;
+                    p = p.Substring(0, p.LastIndexOf(@"\") + 1);
+
+                    webBuilder.UseContentRoot(p);
                     webBuilder.UseStartup<Startup>();
                 });
     }
